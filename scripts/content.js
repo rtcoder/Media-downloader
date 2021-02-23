a766be632e0fc20a7d314fd5cb17c6bb784d20e6
Dawid Jeż <dawidjez@gmail.com>
2021-02-23 20:09:26 +0100

    code refactor

--- scripts/content.js
+++ scripts/content.js
@@ -1,26 +1,31 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    var listElements = document.querySelectorAll(request.search_for);
-    var els = {
-        img: [],
-        video: [],
-        audio: []
-    };
-    [].forEach.call(listElements, function (element) {
-        switch (element.tagName.toLowerCase()) {
-            case 'img':
-                let img = {
-                    src: element.src,
-                    alt: element.alt,
-                    title: element.getAttribute('title')
-                };
-                els.img.push(img);
-                break;
-            case 'video':
-                break;
-            case 'audio':
-                break;
-        }
-    });
-    sendResponse({data: request.data, search: request.search_for, elements: els, success: true});
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const listElements = document.querySelectorAll(request.search_for);
+  const elements = {
+    img: [],
+    video: [],
+    audio: []
+  };
+  [...listElements].forEach(element => {
+    switch (element.tagName.toLowerCase()) {
+      case 'img':
+        let img = {
+          src: element.src,
+          alt: element.alt,
+          title: element.getAttribute('title')
+        };
+        elements.img.push(img);
+        break;
+      case 'video':
+        break;
+      case 'audio':
+        break;
+    }
+  });
+  sendResponse({
+    data: request.data,
+    search: request.search_for,
+    elements,
+    success: true
+  });
 });
 
